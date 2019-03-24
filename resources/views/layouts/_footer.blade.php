35bf270dc0d066b7c878a9bfc3bbfd7446cbb565
jinLong <1475711781@qq.com>
2019-03-24 23:40:50 +0800

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,6 +3,6 @@
         <a href="#">每日一推，每日精进</a>
 
         <div class="float-right">
-            <a href="/about">关于</a>
+            <a href="{{route('about')}}">关于</a>
         </div>
 </footer>
