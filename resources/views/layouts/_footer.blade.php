55ef7402b3d4b50173c02cfe5728a9f36da3b260
jinLong <1475711781@qq.com>
2019-03-24 23:35:51 +0800

    分离头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+        <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+        <a href="#">每日一推，每日精进</a>
+
+        <div class="float-right">
+            <a href="/about">关于</a>
+        </div>
+</footer>
---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a href="/" class="navbar-brand">金龙的博客</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+        </ul>
+    </div>
+</nav>
