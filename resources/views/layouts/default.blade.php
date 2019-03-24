3d8c4d66bf964d106af69efa1615bf0acb16a66f
jinLong <1475711781@qq.com>
2019-03-24 23:21:03 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,8 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>金龙的博客 - @yield('title')</title>
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 <body>
-    @yield('content')
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a href="/" class="navbar-brand">金龙的博客</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,13 @@
 @section('title','首页')
 
 @section('content')
-    <h1>首页</h1>
+    <div class="jumbotron">
+        <h1>欢迎来到金龙的博客</h1>
+        <p class="lead">
+            这里有最好玩的应用推荐，欢迎留言。
+        </p>
+        <p>
+            <a href="#" class="btn btn-lg btn-success" role="button">现在去留言</a>
+        </p>
+    </div>
 @endsection
