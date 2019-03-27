efc65f603412c2372ad668b978415451686383b2
jinLong <1475711781@qq.com>
2019-03-27 11:29:33 +0800

    访问策略

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -7,6 +7,14 @@ use Auth;
 
 class SessionController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('guest',[
+            'only'=>['create']
+        ]);
+    }
+    
     public function create()
     {
         return view('sessions.create');
@@ -21,17 +29,19 @@ class SessionController extends Controller
 
         if(Auth::attempt($credentials,$request->has('remember'))){
             session()->flash('success','欢迎回来');
-            return redirect()->route('users.show',[Auth::user()]);
-        }else{
-            session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
-            return redirect()->back()->withInput();
-        }
+            $fallback = route('users.show',[Auth::user()]);
+            return redirect()->intended($fallback);
+        };
+
+        session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
+        return redirect()->back()->withInput();
+        
     }
 
     public function destory()
     {
         Auth::logout();
         session()->flash('success','退出成功!');
-        return view('sessions.create');
+        return redirect()->route('login');
     }
 }
