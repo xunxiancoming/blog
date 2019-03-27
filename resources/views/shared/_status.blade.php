0a7f0054f13fa30abcdc53b1e763c44d6fa0359b
jinLong <1475711781@qq.com>
2019-03-27 17:18:41 +0800

    destroy blog

--- resources/views/shared/_status.blade.php
+++ resources/views/shared/_status.blade.php
@@ -7,4 +7,12 @@
         <h5 class="mt-0 mb-1">{{$user->name}} <small>/ {{$status->created_at->diffForHumans()}}</small></h5>
         {{$status->content}}
     </div>
+
+    @can('destroy',$status)
+        <form action="{{route('statuses.destroy',$status->id)}}" method="POST">
+            @csrf
+            {{method_field("DELETE")}}
+            <button type="submit" class="btn btn-danger btn-sm">删除</button>
+        </form>
+    @endcan
 </li>
